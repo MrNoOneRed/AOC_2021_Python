5b769924d0805ac9a76d6b38d0b8a7f14f452c63
Piotr Celinski <czaru25@gmail.com>
2022-12-05 01:19:01 +0100

    Add day 2

---
+++ solutions/day2.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+data = []
+
+def parseInput(inputPath):
+    with open(inputPath) as file:
+        for line in file:
+            commands = line.strip().split()
+            data.append({
+                'command': commands[0],
+                'value': commands[1]
+            })
+    return 0
+
+def puzzle1():
+    solution = 0
+    position = 0
+    depth = 0
+
+    for commands in data:
+        if commands['command'] == 'forward':
+            position += int(commands['value'])
+        if commands['command'] == 'down':
+            depth += int(commands['value'])
+        if commands['command'] == 'up':
+            depth -= int(commands['value'])
+
+#         print(commands['command'] + ' - ' + commands['value'])
+
+    solution = position * depth
+
+    return solution
+
+def puzzle2():
+    solution = 0
+    position = 0
+    depth = 0
+    aim = 0
+
+    for commands in data:
+        if commands['command'] == 'forward':
+            position += int(commands['value'])
+            depth += aim * int(commands['value'])
+        if commands['command'] == 'down':
+            aim += int(commands['value'])
+        if commands['command'] == 'up':
+            aim -= int(commands['value'])
+
+    solution = position * depth
+
+    return solution
